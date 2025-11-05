a0e5a2be4b4858500d7e2c74fadcdcbd4feea4ae
fakhrifawwaz <email@fakhri07fawwaz@gmai.com>
2025-11-05 10:30:54 +0700

    modifikasi 2 latihan 2

--- ArrayNilai15.java
+++ ArrayNilai15.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 
 public class ArrayNilai15 {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
-        int [] nilaiAkhir = new int [10];
+        int[] nilaiAkhir = new int[10];
 
-        for (int i = 0 ; i < nilaiAkhir.length; i++){
-            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
-            nilaiAkhir [i] = sc.nextInt();
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+            nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0 ; i < nilaiAkhir.length; i++){
+
+        System.out.println("\n=== HASIL KELULUSAN MAHASISWA ===");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
-                System.out.println("Mahasiswa ke-"+i+" lulus dengan nilai "+nilaiAkhir[i]);
+                System.out.println("Mahasiswa ke-" + i + " : " + nilaiAkhir[i] + " → LULUS");
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " : " + nilaiAkhir[i] + " → TIDAK LULUS");
             }
         }
+
         sc.close();
     }
 }
